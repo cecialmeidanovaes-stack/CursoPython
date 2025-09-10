55d69e5aa19d14e0348c0f61569c12af66fc1102
pilha cecialmeidanovaes <cecialmeidanovaes@gmail.com>
2025-09-09 21:22:07 -0300

    teste

---
+++ aula1.py
@@ -0,0 +1,26 @@
+# Primeiro programa.py
+
+# print("Oi, seja bem vindo ao curso Python!")
+
+# print(11+10+1000)
+# print(1.5+1+1.5)
+# print(True)
+# print(False)
+# print("Python")
+
+# int()
+
+# print(int(1.90002055))
+# print(int("10"))
+# print(float("10.10"))
+# print(float(100))
+
+# valor = 10
+# valor_str = str(valor)
+# print(valor)
+
+nome = input("Informe seu nome: ")
+idade = input("informe sua idade: ")
+
+print(nome, idade)
+print(nome, idade)
---
+++ aula2.py
@@ -0,0 +1,21 @@
+
+#variavel
+idade = 25
+mensagem = "Bem vindo ao curso de Pyton"
+nome = "Mariana"
+#saida
+print (idade)
+print (mensagem)
+
+#funçãpp com retorno explicito
+
+print (f"olá", nome, "Sejam bem-vindo(a).")
+
+#Função para verificar se o número inteiro é par ou impar.
+numero = int(input ("Digite um número: "))
+
+if numero % 2 == 0:
+    print ("Par")
+else:
+    print ("impar")
+
